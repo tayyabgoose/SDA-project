f80646f59e489eb5d8fbb48af99acd0f09775a9c
Muhammad Tayyab <40181908+tayyabgoose@users.noreply.github.com>
2024-04-27 19:56:29 +0500

    file population

--- filehandling.cpp
+++ filehandling.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include<vector>
-#include<sstream>
+#include <vector>
+#include <sstream>
 
 using namespace std;
 
@@ -10,49 +10,64 @@ int main()
 {
     fstream file;
     file.open("file.csv");
-    string temp="";
-    string line="";
-    string word=" ";
+    string temp = "";
+    string line = "";
+    string word = " ";
 
-    getline(file,line);
+    getline(file, line);
 
-
-    vector<int> PloV;
-    vector<string> ProV;
-    vector<int> CloV;
-    vector<string> CouV;
-    vector<string> EvalV;
-    vector<int> QV;
-
+    vector<int> PLO_vector;
+    vector<string> program_vector;
+    vector<int> CLO_vector;
+    vector<string> course_vector;
+    vector<string> evaluation_vector;
+    vector<int> question_vector;
 
     while (getline(file, line))
     {
         stringstream ss(line);
         string word;
         int index = 0;
-        while (getline(ss, word, ',')) {
-            if (index == 0) {
-                PloV.push_back(stoi(word));
-            } else if (index == 1) {
-                ProV.push_back(word);
-            } else if (index == 2) {
-                CloV.push_back(stoi(word));
-            } else if (index == 3) {
-                CouV.push_back(word);
-            } else if (index == 4) {
-                EvalV.push_back(word);
-            } else if (index == 5) {
-                QV.push_back(stoi(word));
+        while (getline(ss, word, ','))
+        {
+            if (index == 0)
+            {
+                PLO_vector.push_back(stoi(word));
+            }
+            else if (index == 1)
+            {
+                program_vector.push_back(word);
+            }
+            else if (index == 2)
+            {
+                CLO_vector.push_back(stoi(word));
+            }
+            else if (index == 3)
+            {
+                course_vector.push_back(word);
+            }
+            else if (index == 4)
+            {
+                evaluation_vector.push_back(word);
+            }
+            else if (index == 5)
+            {
+                question_vector.push_back(stoi(word));
             }
             index++;
         }
     }
     cout << endl;
-    for (int i = 0; i < PloV.size(); ++i) {
-        cout <<"Plos:"<< PloV[i] << " "<<"Programs:"<< ProV[i] << " "<<"Clos:"<< CloV[i] << " "<<"Courses:"<< CouV[i] << " "<<"Evaluations:"<< EvalV[i] << " "<<"Questions:"<< QV[i] <<endl;
+    for (int i = 0; i < PLO_vector.size(); ++i)
+    {
+        cout << "PLO: " << PLO_vector[i] << " "
+             << "Program: " << program_vector[i] << " "
+             << "CLO: " << CLO_vector[i] << " "
+             << "Courses: " << course_vector[i] << " "
+             << "Evaluations: " << evaluation_vector[i] << " "
+             << "Questions: " << question_vector[i] << endl;
     }
     cout << endl;
-
 
     file.close();
     return 0;
